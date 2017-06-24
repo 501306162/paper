f1bbefcaed33b45059d74e165494212545abc5e1
501306162 <501306162@qq.com>
2017-06-24 11:44:24 +0800

    ...

--- lbfgs/lbfgs/minFunc_comprehension/movepixels_2d.m
+++ lbfgs/lbfgs/minFunc_comprehension/movepixels_2d.m
@@ -1,4 +1,4 @@
-function Iout=movepixels_2d(Iin,Tx,Ty,mode)
+function Iout=movepixels_2d(Iin,Tx,Ty)
 % This function movepixels, will translate the pixels of an image
 %  according to x and y translation images (bilinear interpolated). 
 % 
@@ -7,11 +7,7 @@ function Iout=movepixels_2d(Iin,Tx,Ty,mode)
 % Inputs;
 %   Tx, Ty: The transformation images, describing the
 %             (backwards) translation of every pixel in x and y direction.
-%   mode: If 0: linear interpolation and outside pixels set to nearest pixel
-%            1: linear interpolation and outside pixels set to zero
-%            (cubic interpolation only supported by compiled mex file)
-%            2: cubic interpolation and outsite pixels set to nearest pixel
-%            3: cubic interpolation and outside pixels set to zero
+%   mode:   linear interpolation and outside pixels set to nearest pixel
 %
 % Outputs,
 %   Iout : The transformed image
@@ -54,18 +50,12 @@ yBas1(check_yBas1)=0;
 Iout=zeros(size(Iin));
   
 Iin_one=Iin(:,:);
-Get the intensities
+% Get the intensities
 intensity_xyz0=Iin_one(1+xBas0+yBas0*size(Iin,1));
 intensity_xyz1=Iin_one(1+xBas0+yBas1*size(Iin,1)); 
 intensity_xyz2=Iin_one(1+xBas1+yBas0*size(Iin,1));
 intensity_xyz3=Iin_one(1+xBas1+yBas1*size(Iin,1));
-% % Make pixels before outside Ibuffer mode
-% if(mode==1||mode==3)
-%     intensity_xyz0(check_xBas0|check_yBas0)=0;
-%     intensity_xyz1(check_xBas0|check_yBas1)=0;
-%     intensity_xyz2(check_xBas1|check_yBas0)=0;
-%     intensity_xyz3(check_xBas1|check_yBas1)=0;
-% end
+
 Iout_one=intensity_xyz0.*perc0+intensity_xyz1.*perc1+intensity_xyz2.*perc2+intensity_xyz3.*perc3;
 Iout(:,:)=reshape(Iout_one, [size(Iin,1) size(Iin,2)]);
 
